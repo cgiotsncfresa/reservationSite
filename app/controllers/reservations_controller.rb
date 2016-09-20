255f6388754342c9b66a862efb694539daa065b4
cgiotsncfresa <cgiotsncfresa@laposte.net>
2016-09-20 17:26:52 +0200

    Retrait de la vérification crsf

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,7 @@ require 'oj'
 
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   # GET /reservations
   # GET /reservations.json
